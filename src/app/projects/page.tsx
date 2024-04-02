9d709f003016b6a685ba1611aaf6f69310e05750
Axizeaus <axizeaus@gmail.com>
2024-04-02 19:58:35 +0630

    CRUD for programming languages util

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,12 +15,6 @@ import {
 } from "@/lib/frameworkUtils";
 
 export default async function Page() {
-  // console.log(await getAllProgrammingLanguages());
-  // console.log(await getProgrammingLanguageById(1));
-  // const updatedPL = await updateProgrammingLanguage(1, "Python3");
-  await deleteProgrammingLanguage(1);
-  console.log(await getAllProgrammingLanguages());
-
   return (
     <h2>This is project, check console to see if the project is created</h2>
   );
